e5f7392ac51460912c35588b9a53d223b5879722
Daniel Rhodes <danielrhodes47@gmail.com>
2024-08-25 13:32:16 -0500

    updated UI, incorporated handlePause/play asthetic.

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -3,10 +3,16 @@ import { useDispatch } from 'react-redux';
 import PlayPause from './PlayPause';
 import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
-const SongCard = ({ song, i }) => {
-  console.log('Song Data:', song);
-  const activeSong = 'Test';
+const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
+  console.log(song)
 
+  const handlePauseClick = () => {
+
+  };
+
+  const handlePLayClick = () => {
+
+  };
 
   return (
     < div className='flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer group' >
@@ -15,19 +21,26 @@ const SongCard = ({ song, i }) => {
           className={`absolute inset-0 flex justify-center items-center bg-black bg-opacity-50 transition-opacity duration-300 ease-in-out ${activeSong === song.attributes.name ? 'flex bg-black bg-opacity-70' : 'hidden'
             } ${'group-hover:flex group-hover:bg-black group-hover:bg-opacity-70'}`}
         >
-          <PlayPause />
+          <PlayPause
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            song={song}
+            handlePause={handlePauseClick}
+            handlePlay={handlePLayClick}
+          />
         </div>
         <img alt="song_img" src={song.attributes?.artwork?.url} />
       </div>
       <div className="mt-4 flex flex-col">
 
-        <p>
-          <Link>
+        <p className="font-semibold text-lg text-white truncate ">
+          <Link to={`/songs/${song?.key}`}>
             {song.attributes.name}
           </Link>
         </p>
-        <p>
-          <Link>
+        <p className="text-sm truncate text-gray-300 mt-1">
+
+          <Link to={song.artists ? `/artists/${song?.artists[0]?.adamid}` : '/top-artists'}>
             {song.attributes.artistName}
           </Link>
         </p>
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,8 +1,12 @@
+import { useDispatch, useSelector } from 'react-redux';
+
 import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const Discover = () => {
+    const dispatch = useDispatch();
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
     const countryCode = 'US'
     const { data, isFetching, error } = useGetTopChartsQuery(countryCode);
     const genreTitle = 'Pop';
@@ -31,6 +35,9 @@ const Discover = () => {
                     <SongCard
                         key={song.key}
                         song={song}
+                        isPlaying={isPlaying}
+                        activeSong={activeSong}
+                        data={data}
                         i={i}
                     />
                 ))}
