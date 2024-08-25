cc24a0d0e6ecdc9385b937689b0874d1c5c4894b
Daniel Rhodes <danielrhodes47@gmail.com>
2024-08-25 12:02:18 -0500

    polished songCards and called correct attribuetes for name and artist name

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,7 +1,39 @@
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-const SongCard = () => (
-  <div>SongCard</div>
-);
+import PlayPause from './PlayPause';
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
+
+const SongCard = ({ song, i }) => {
+  console.log('Song Data:', song);
+  const activeSong = 'Test';
+
+
+  return (
+    < div className='flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer group' >
+      <div className='relative w-full h-56'>
+        <div
+          className={`absolute inset-0 flex justify-center items-center bg-black bg-opacity-50 transition-opacity duration-300 ease-in-out ${activeSong === song.attributes.name ? 'flex bg-black bg-opacity-70' : 'hidden'
+            } ${'group-hover:flex group-hover:bg-black group-hover:bg-opacity-70'}`}
+        >
+          <PlayPause />
+        </div>
+        <img alt="song_img" src={song.attributes?.artwork?.url} />
+      </div>
+      <div className="mt-4 flex flex-col">
+
+        <p>
+          <Link>
+            {song.attributes.name}
+          </Link>
+        </p>
+        <p>
+          <Link>
+            {song.attributes.artistName}
+          </Link>
+        </p>
+      </div>
+    </div >
+  );
+};
 
 export default SongCard;
